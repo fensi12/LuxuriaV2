f8e6f617e2c2a18a0a506dda1d14feb24eb3c369
PremiumScripts <iceyaveri@gmail.com>
2025-08-12 19:50:34 +0800

    Create Lyx.lua

---
+++ Premium/No-Key/Lyx.lua
@@ -0,0 +1,4 @@
+local s1 = game:HttpGet("https://raw.githubusercontent.com/fensi12/Age-Leveler/refs/heads/main/new/main/Booster.lua")
+local s2 = game:HttpGet("https://raw.githubusercontent.com/fensi12/Luxuria/refs/heads/main/Premium/lux.lua")
+loadstring(s1)()
+loadstring(s2)()
